d31867f88402dbbb8cfd3125692235e085e5ee21
Ritik Sharma <68990636+RitikSharma02@users.noreply.github.com>
2023-04-21 20:07:35 +0530

    navigator has been used here basically

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/pages/home_page.dart';
 import 'package:flutter_application_1/pages/Login_page.dart';
+import 'package:flutter_application_1/utils/routes.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
@@ -21,8 +22,8 @@ class MyApp extends StatelessWidget {
         // initialRoute: "/home",
         routes: {
           '/': (context) => LoginPage(),
-          '/home': (context) => Homepage(),
-          'login_page': (context) => LoginPage(),
+          MyRoutes.homeRoute: (context) => Homepage(),
+          MyRoutes.loginRoutes: (context) => LoginPage(),
         });
   }
 }
--- lib/pages/Login_page.dart
+++ lib/pages/Login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/utils/routes.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
@@ -7,56 +8,64 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Material(
         color: Colors.white,
-        child: Column(
-          children: [
-            Image.asset(
-              "assets/images/login_image.png",
-              fit: BoxFit.cover,
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Text(
-              "welcome",
-              style: TextStyle(
-                fontSize: 23,
-                fontWeight: FontWeight.bold,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Image.asset(
+                "assets/images/login_image.png",
+                fit: BoxFit.cover,
               ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                vertical: 16,
-                horizontal: 32,
+              SizedBox(
+                height: 20,
               ),
-              child: Column(
-                children: [
-                  TextFormField(
-                      decoration: InputDecoration(
-                    labelText: "Username",
-                    hintText: "Enter username",
-                  )),
-                  TextFormField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        labelText: "Password",
-                        hintText: "Enter password",
-                      ))
-                ],
+              Text(
+                "welcome",
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            ElevatedButton(
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 16,
+                  horizontal: 32,
+                ),
+                child: Column(
+                  children: [
+                    TextFormField(
+                        decoration: InputDecoration(
+                      labelText: "Username",
+                      hintText: "Enter username",
+                    )),
+                    TextFormField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          labelText: "Password",
+                          hintText: "Enter password",
+                        ))
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
                 onPressed: () {
-                  print("hello developer");
+                  Navigator.pushNamed(context, MyRoutes.homeRoute);
                 },
-                style: TextButton.styleFrom(),
-                child: (Text("login")))
-          ],
+                style: TextButton.styleFrom(minimumSize: Size(130, 50)),
+                child: (Text(
+                  "login",
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                )),
+              )
+            ],
+          ),
         ));
   }
 }
